7eab8c91926b5a582d332d0de6d502af9900252b
mohamed-elbayomy <mohamedelbauomy616@gmial.com>
2023-09-30 22:30:58 +0300

    .

--- app/src/main/java/com/example/unitedmariners/login_screen.java
+++ app/src/main/java/com/example/unitedmariners/login_screen.java
@@ -1,19 +1,28 @@
 package com.example.unitedmariners;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login_screen extends AppCompatActivity {
   EditText et_email,et_password;
   TextView tv_forget_password;
   Button btn_login,btn_create_an_account,btn_facebook,btn_google;
 
+  FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
 
   @Override
@@ -28,13 +37,53 @@ public class login_screen extends AppCompatActivity {
     btn_login = findViewById(R.id.btn_login);
     btn_create_an_account = findViewById(R.id.btn_create_an_account);
     btn_google = findViewById(R.id.btn_google);
-    btn_login.setOnClickListener(new View.OnClickListener() {
+
+
+
+    btn_create_an_account.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Intent intent=new Intent(getApplicationContext(), MapsActivity.class);
+        Intent intent = new Intent(getApplicationContext(), sign_up_screen.class);
         startActivity(intent);
       }
     });
+
+    btn_login.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String email, password;
+        email = String.valueOf(et_email.getText());
+        password = String.valueOf(et_password.getText());
+
+        if (TextUtils.isEmpty(email)){
+          Toast.makeText(login_screen.this,"Enter email",Toast.LENGTH_SHORT).show();
+          return;
+        }
+        if (TextUtils.isEmpty(password)){
+          Toast.makeText(login_screen.this,"enter password",Toast.LENGTH_SHORT).show();
+          return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                  @Override
+                  public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                      Toast.makeText(login_screen.this,"login successful",Toast.LENGTH_SHORT).show();
+                      Intent intent = new Intent(login_screen.this,MapsActivity.class);
+                      startActivity(intent);
+                      finish();
+                    }
+                    else {
+                      Toast.makeText(login_screen.this,"failled",Toast.LENGTH_SHORT).show();
+
+                    }
+                  }
+                });
+
+      }
+    });
+
 
   }
 }
--- app/src/main/java/com/example/unitedmariners/sign_up_screen.java
+++ app/src/main/java/com/example/unitedmariners/sign_up_screen.java
@@ -1,15 +1,28 @@
 package com.example.unitedmariners;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class sign_up_screen extends AppCompatActivity {
   EditText et_user_name,s_et_email,s_et_password,et_verify_password;
   TextView s_tv_login;
   Button btn_signup,s_btn_login;
+
+  FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -21,5 +34,51 @@ public class sign_up_screen extends AppCompatActivity {
 
     btn_signup = findViewById(R.id.btn_signup);
     s_tv_login = findViewById(R.id.s_tv_login);
+
+    s_tv_login.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), login_screen.class);
+        startActivity(intent);
+      }
+    });
+
+
+    btn_signup.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String email, password;
+        email = String.valueOf(s_et_email.getText());
+        password = String.valueOf(s_et_password.getText());
+
+        if (TextUtils.isEmpty(email)){
+          Toast.makeText(sign_up_screen.this,"Enter email",Toast.LENGTH_SHORT).show();
+          return;
+        }
+        if (TextUtils.isEmpty(password)){
+          Toast.makeText(sign_up_screen.this,"enter password",Toast.LENGTH_SHORT).show();
+          return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                  @Override
+                  public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                      Toast.makeText(sign_up_screen.this,"login successful",Toast.LENGTH_SHORT).show();
+                      Intent intent = new Intent(sign_up_screen.this,MapsActivity.class);
+                      startActivity(intent);
+                      finish();
+                    }
+                    else {
+                      Toast.makeText(sign_up_screen.this,"failled",Toast.LENGTH_SHORT).show();
+
+                    }
+                  }
+                });
+
+      }
+    });
+
   }
 }
