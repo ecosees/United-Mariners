9d3f2c7eac392819a38fdd140b8e1150e924d7fb
Khedr <khedrkhedr370@gmail.com>
2023-10-01 15:16:46 +0300

    fireBase

--- app/src/main/java/com/example/unitedmariners/ProfileFragment.java
+++ app/src/main/java/com/example/unitedmariners/ProfileFragment.java
@@ -1,19 +1,45 @@
 package com.example.unitedmariners;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProfileFragment extends Fragment {
 
+TextView logOut,emailTxt;
+FirebaseAuth auth;
+FirebaseUser user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v=inflater.inflate(R.layout.fragment_profile, container, false);
+        logOut=v.findViewById(R.id.logout);
+        emailTxt=v.findViewById(R.id.user_email);
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
+        if(user==null){
+           Intent intent = new Intent(getContext(), login_screen.class);
+            startActivity(intent);
+        }else {
+            emailTxt.setText(user.getEmail());
+        }
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), login_screen.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
